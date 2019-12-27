14e203aeaf09b01f824acbdd54e23bb1a2447cf2
SO-JIMM <jamesdgrace@gmail.com>
2019-12-28 00:18:51 +1100

    Updates

---
+++ precache-manifest.a9b46f6078c00d5c1e7c599669cb5cc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed6348aa648832afc01d7a6cebbade76",
+    "url": "/,io/nicked-name/index.html"
+  },
+  {
+    "revision": "791d51a61e1a344c7981",
+    "url": "/,io/nicked-name/static/css/main.88ea5f15.chunk.css"
+  },
+  {
+    "revision": "49226eda11545851eb3c",
+    "url": "/,io/nicked-name/static/js/2.a0777f49.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/,io/nicked-name/static/js/2.a0777f49.chunk.js.LICENSE"
+  },
+  {
+    "revision": "791d51a61e1a344c7981",
+    "url": "/,io/nicked-name/static/js/main.18f78ba1.chunk.js"
+  },
+  {
+    "revision": "cf5f2ab1c6bd6c036a0c",
+    "url": "/,io/nicked-name/static/js/runtime-main.ab26491f.js"
+  }
+]);
